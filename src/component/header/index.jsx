3d481b0bcc29051925133e4c35d41eca42416ccc
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-02-19 11:16:59 +0330

    fix: fix a bug in showing cart

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -1,13 +1,13 @@
 import {FaShoppingCart} from "react-icons/fa"
 import { useSelector } from "react-redux"
 import "./style.css"
-const Header = ({showCart,setShowCart}) => {
+const Header = ({setShowCart}) => {
 const totalQuantity = useSelector(state => state.cart.cartTotalQuantity)
 return (
 <div className="header">
-<button onClick={()=> setShowCart(!showCart)}  className="p-4">store</button>
+<button onClick={()=> setShowCart(false)}  className="p-4">store</button>
 <button  className="p-4">about</button>
-<button onClick={()=> setShowCart(!showCart)}><FaShoppingCart/>cart ({totalQuantity})</button>
+<button onClick={()=> setShowCart(true)}><FaShoppingCart/>cart ({totalQuantity})</button>
 <div>
     <h1>Store</h1>
     <p>This is a store page</p>
